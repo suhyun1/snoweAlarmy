a9cd77f0cd6c8e258d1b6e983c021f7e2a31ab76
suhyun1 <suhyun7079@gmail.com>
2020-08-13 16:18:35 +0900

    feat: 취업관련 공지 추가

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ require("dotenv").config();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 const chatId = '@snowenotice';  //채널id
 
-let prevPost = [];
-let curPost = [];
+let preNotice = [];
+let curNotice = [];
+let preCareerNotice = [];
+let curCareerNotice = [];
 
-const pushMessage = (chatId) => {
- 
-    const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/notice/"+curPost[0].id;
-    bot.sendMessage(chatId, `새로운 공지사항이 있습니다.\n${curPost[0].title}\n${url}`); 
-}
+// const pushMessage = (chatId) => {
+
+//     const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/notice/"+curNotice[0].id;
+//     bot.sendMessage(chatId, `새로운 공지사항이 있습니다.\n${curNotice[0].title}\n${url}`); 
+// }
 
 const getNewPosts = async() => {
 
@@ -34,10 +36,28 @@ const getNewPosts = async() => {
     await page.click("#loginButton");
     console.log("login success");
 
+    //공지사항
     await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/notice");
     await page.waitFor(3000);
 
-    curPost = await page.evaluate(() => {
+    curNotice = await page.evaluate(() => {
+      const tbodyChilds = document.querySelector("#messageListBody").children; 
+      let posts = [];
+      for (let i = 0; i < tbodyChilds.length; i++) {
+        if (tbodyChilds[i].children.length != 5) continue;
+        posts.push({
+          id: tbodyChilds[i].attributes[0].value.substring(3),  //게시물 id
+          title: tbodyChilds[i].children[2].textContent,        //게시물 제목
+        });
+      }
+      return posts;
+    });
+
+    //취업관련 공지(별도 게시판에 존재함)
+    await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer");
+    await page.waitFor(3000);
+
+    curCareerNotice = await page.evaluate(() => {
       const tbodyChilds = document.querySelector("#messageListBody").children; 
       let posts = [];
       for (let i = 0; i < tbodyChilds.length; i++) {
@@ -52,16 +72,28 @@ const getNewPosts = async() => {
 
     await browser.close();
 
-    if (prevPost.length > 0 && curPost[0].id !== prevPost[0].id) {
-      console.log("update!");
-      pushMessage(chatId);
+    if (preNotice.length > 0 && curNotice[0].id !== preNotice[0].id) {
+      console.log("notice update!");
+      const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/notice/" + curNotice[0].id;
+      const title = curNotice[0].title;
+      bot.sendMessage(chatId, `새로운 공지사항이 있습니다.\n${title}\n${url}`); 
+      // pushMessage(chatId);
+    }
+
+    if (preCareerNotice.length > 0 && curCareerNotice[0].id !== preCareerNotice[0].id) {
+      console.log("career notice update!");
+      const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer/" + curCareerNotice[0].id;
+      const title = curCareerNotice[0].title;
+      bot.sendMessage(chatId, `새로운 취업경력개발 게시물이 있습니다.\n${title}\n${url}`); 
+      // pushMessage(chatId);
     }
 }
 
 const job = new cron.schedule( "*/30 6-20 * * 1-5", function () {
       let today = new Date();
       console.log(today);
-      prevPost = curPost;
+      preNotice = curNotice;
+      preCareerNotice = curCareerNotice;
       getNewPosts();
   }, null, true, "Asia/Seoul"
 );
