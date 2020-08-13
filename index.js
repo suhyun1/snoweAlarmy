f5c85ffde8804688f54c941e68b785c9e017afe2
suhyun1 <suhyun7079@gmail.com>
2020-08-13 23:45:18 +0900

    feat: 오늘 날짜에 올라온 게시물 한 번에 보여주도록 수정
    기존에는 30분에 한번씩 읽어오기 위해 node-cron사용했으나
    업로드 게시물이 적어 하루에 한 번 모아서 알람을 해주기로 수정

--- index.js
+++ index.js
@@ -6,104 +6,92 @@ require("dotenv").config();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 const chatId = '@snowenotice';  //채널id
 
-let preNotice = [];
-let curNotice = [];
-let preCareerNotice = [];
-let curCareerNotice = [];
+const getFormatDate = async(date) => {
+     const year = date.getFullYear();
+     const month = 1 + date.getMonth();
+     const day = date.getDate();
+     console.log(`day:${day}`);
+     return `${year.toString().substring(2, 4)}.${month >= 10 ? month : "0" + month}.${day >= 10 ? day : "0" + day}`;
+}; //YY.MM.DD 형식
 
-// const pushMessage = (chatId) => {
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-//     const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/notice/"+curNotice[0].id;
-//     bot.sendMessage(chatId, `새로운 공지사항이 있습니다.\n${curNotice[0].title}\n${url}`); 
-// }
+  await page.goto("https://snowe.sookmyung.ac.kr/bbs5/users/login");
 
-const getNewPosts = async() => {
+  await page.evaluate(
+    (id, pw) => {
+      document.querySelector("#userId").value = id;
+      document.querySelector("#userPassword").value = pw;
+    },
+    process.env.USER_ID,
+    process.env.USER_PW
+  );
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  await page.click("#loginButton");
+  console.log("login success");
 
-    await page.goto("https://snowe.sookmyung.ac.kr/bbs5/users/login");
-
-    await page.evaluate(
-      (id, pw) => {
-        document.querySelector("#userId").value = id;
-        document.querySelector("#userPassword").value = pw;
-      },
-      process.env.USER_ID,
-      process.env.USER_PW
-    );
-
-    await page.click("#loginButton");
-    console.log("login success");
-
-    //공지사항
-    await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/notice");
-    await page.waitFor(3000);
-
-    curNotice = await page.evaluate(() => {
-      const tbodyChilds = document.querySelector("#messageListBody").children; 
-      let posts = [];
-      for (let i = 0; i < tbodyChilds.length; i++) {
-        if (tbodyChilds[i].children.length != 5) continue;
-        posts.push({
-          id: tbodyChilds[i].attributes[0].value.substring(3),  //게시물 id
-          title: tbodyChilds[i].children[2].textContent,        //게시물 제목
-        });
-      }
-      return posts;
-    });
-
-    //취업관련 공지(별도 게시판에 존재함)
-    await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer");
-    await page.waitFor(3000);
-
-    curCareerNotice = await page.evaluate(() => {
-      const tbodyChilds = document.querySelector("#messageListBody").children; 
-      let posts = [];
-      for (let i = 0; i < tbodyChilds.length; i++) {
-        if (tbodyChilds[i].children.length != 5) continue;
-        posts.push({
-          id: tbodyChilds[i].attributes[0].value.substring(3),  //게시물 id
-          title: tbodyChilds[i].children[2].textContent,        //게시물 제목
-        });
-      }
-      return posts;
-    });
-
-    await browser.close();
-
-    if (preNotice.length > 0 && curNotice[0].id !== preNotice[0].id) {
-      console.log("notice update!");
-      const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/notice/" + curNotice[0].id;
-      const title = curNotice[0].title;
-      bot.sendMessage(chatId, `새로운 공지사항이 있습니다.\n${title}\n${url}`); 
-      // pushMessage(chatId);
+  //공지사항
+  await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/notice");
+  await page.waitFor(3000);
+  // today.setHours(today.getHours()+9);
+  const notice = await page.evaluate(() => {
+    let posts = [];
+    const tbodyChilds = document.querySelector("#messageListBody").children;
+    for (let i = 0; i < tbodyChilds.length; i++) {
+      if (tbodyChilds[i].children.length !== 5) continue;
+      posts.push({
+        id: tbodyChilds[i].attributes[0].value.substring(3), //게시물 id
+        title: tbodyChilds[i].children[2].textContent, //게시물 제목
+        createdAt: tbodyChilds[i].children[4].textContent.substring(0, 8), //게시물 생성날짜
+      });
     }
+    return posts;
+  });
 
-    if (preCareerNotice.length > 0 && curCareerNotice[0].id !== preCareerNotice[0].id) {
-      console.log("career notice update!");
-      const url = "https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer/" + curCareerNotice[0].id;
-      const title = curCareerNotice[0].title;
-      bot.sendMessage(chatId, `새로운 취업경력개발 게시물이 있습니다.\n${title}\n${url}`); 
-      // pushMessage(chatId);
+  //취업관련 공지(별도 게시판에 존재함)
+  await page.goto("https://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer");
+  await page.waitFor(3000);
+
+  const careerNotice = await page.evaluate(() => {
+    let posts = [];
+    const tbodyChilds = document.querySelector("#messageListBody").children;
+    for (let i = 0; i < tbodyChilds.length; i++) {
+      if (tbodyChilds[i].children.length !== 5) continue;
+      posts.push({
+        id: tbodyChilds[i].attributes[0].value.substring(3), //게시물 id
+        title: tbodyChilds[i].children[2].textContent, //게시물 제목
+        createdAt: tbodyChilds[i].children[4].textContent.substring(0, 8), //게시물 생성날짜
+      });
     }
-}
+    return posts;
+  });
 
-const job = new cron.schedule( "*/30 6-20 * * 1-5", function () {
-      let today = new Date();
-      console.log(today);
-      preNotice = curNotice;
-      preCareerNotice = curCareerNotice;
-      getNewPosts();
-  }, null, true, "Asia/Seoul"
-);
+  await browser.close();
 
+  const todayDate = await getFormatDate(new Date());
+  
+  if (notice.length > 0) {
+    let message = `💌오늘의 공지사항입니다.\n`;
+    for (let i = 0; i < notice.length; i++) {
+      if (notice[i].createdAt !== todayDate) continue;
+      message += `👉${notice[i].title.trim()}\nhttps://snowe.sookmyung.ac.kr/bbs5/boards/notice/${notice[i].id}\n`;
+    }
+    await bot.sendMessage(chatId, message);
+  }
 
-bot.sendMessage(
-  chatId,
-  "공지사항에 새 글이 올라오면 알려드립니다.\n월~금 6시~21시 30분 간격으로 업데이트를 확인합니다."
-);
-job.start();
+  if (careerNotice.length > 0) {
+    let message = `💌오늘의 취업경력개발 게시글입니다.\n`;
+    for (let i = 0; i < careerNotice.length; i++) {
+      if (careerNotice[i].createdAt !== todayDate) continue;
+      message += `👉${careerNotice[i].title.trim()}\nhttps://snowe.sookmyung.ac.kr/bbs5/boards/jobcareer/${careerNotice[i].id}\n`;
+    }
+    await bot.sendMessage(chatId, message);
+  }
+})().catch((e) => {
+  console.error(e.stack); 
+});
 
 
 
